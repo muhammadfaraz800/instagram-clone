a58a608591b9f2931d3e4c3fbd6748c9de09e821
Muhammad Faraz <muhammadfaraz800@gmail.com>
2025-11-19 19:31:37 +0500

    Added index.html as basic homepage
    remove sending hello world from index.js

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ app.use(express.json());
 app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 app.use('/', router);
 
