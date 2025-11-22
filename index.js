88fd7f68a3fd8960d3c7a5670a484637a6e41352
Muhammad Faraz <muhammadfaraz800@gmail.com>
2025-11-22 10:08:51 +0500

    More organized API endpoints
    Added /api/signup and /api/login routes for better user management.
    reduced image height in style.css to 521px

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import router from './route.js';
 const app = express();
 
-app.use(express.json());
-app.use(express.static('public'));
+app.use(express.json()); //middleware to parse JSON bodies
+app.use(express.static('public')); //middleware to serve static files
 const PORT = process.env.PORT || 3000;
 
 
-app.use('/', router);
+app.use('/api', router); //middleware to handle api routes
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
